8ce7c973f455caafbef801e5779c3d0548046901
Panasa Teja <panasateja123@gmail.com>
2026-08-28 15:05:37 +0530

    Use keyless OpenStreetMap tiles
    Remove the API-key-dependent basemap so the public app remains free and immediately usable.

--- src/App.tsx
+++ src/App.tsx
@@ -864,9 +864,10 @@ function App() {
             className="map"
           >
             <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-              url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
               maxZoom={19}
+              className="base-map-tiles"
             />
             <MapController focus={focus} mapRef={mapRef} />
             <MapClickHandler enabled={isAdding} onAdd={addZone} />
